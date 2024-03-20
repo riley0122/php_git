69224650e5cf76a22cf2b87729c9e24f4947a573
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-20 15:21:42 +0100

    Added pulling the data from subdirectory to wwwroot

--- server/config.php
+++ server/config.php
@@ -1,6 +1,6 @@
 <?php
-define('GIT_URL', '');
+define('GIT_URL', 'https://github.com/riley0122/php_git');
 define('FOLDER', './');
-define('BRANCH', 'main');
+define('BRANCH', '1-create-the-loader');
 define('KEY', 'RANDOM_KEY');
 ?>
--- server/loadGit.php
+++ server/loadGit.php
@@ -42,12 +42,67 @@ function rmdir_recursive($dirname) {
     }
 }
 
+function extractRepoName($repoUrl) {
+    // Trim any leading or trailing whitespace
+    $repoUrl = trim($repoUrl);
+
+    // Check if the URL ends with ".git"
+    if (str_ends_with($repoUrl, '.git')) {
+        // Remove the ".git" extension from the URL[]
+        $repoUrl = substr($repoUrl, 0, -4);
+    }
+
+    // Check if the URL starts with "http://" or "https://"
+    if (!str_starts_with($repoUrl, 'https://') && !str_ends_with($repoUrl,"http://")) {
+        throw new BadFunctionCallException("extractRepoName() expects parameter 1 to start with https:// or http://");
+    }
+
+    // Extract the repo name from the URL
+    $repoName = substr($repoUrl, strrpos($repoUrl, '/') + 1);
+
+    return $repoName;
+}
+
+function pullSubData($subDirectoryName, $extraPath = "") {
+    if (!is_dir($subDirectoryName)) {
+        throw new BadFunctionCallException("pullSubData() expects parameter 1 to be a directory");
+    }
+
+    // For every file in the sub directory
+    foreach (scandir($subDirectoryName . "/" . $extraPath) as $file) {
+        if ($file!= "." && $file!= "..") {
+            if (is_dir($subDirectoryName . $extraPath . "/" . $file)) {
+                if (!is_dir($subDirectoryName . "/\..\/" . $extraPath . "/" . $file)) {
+                    mkdir($subDirectoryName . "/\..\/" .  $extraPath . "/" . $file);
+                }
+                pullSubData($subDirectoryName, $extraPath . $file);
+            } else {
+                // If the file isn't a sub directory, copy the file to the main directory
+                $filePath = $subDirectoryName . "/" . $extraPath . "/" . $file;
+                $fileContents = file_get_contents($filePath);
+
+                $newFilePath = $subDirectoryName . "\/..\/" . $extraPath . "/" . $file;
+
+                file_put_contents($newFilePath, $fileContents);
+            }
+        }
+    }
+
+    // Remove the sub directory if this is the root sub directory
+    if($extraPath == ""){
+        rmdir_recursive($subDirectoryName. "/". $extraPath);
+    }
+
+    return true;
+}
+
 function unpackGitZip($zip_data) {
     // Delete current wwwdata directory
     if (is_dir("wwwdata")) {
         rmdir_recursive("wwwdata");
     }
 
+    // Delete the temorary files
     if (file_exists("wwwdata.tar.gz")) {
         unlink("wwwdata.tar.gz");
     }
@@ -70,6 +125,11 @@ function unpackGitZip($zip_data) {
     // Delete the temporary files
     unlink("wwwdata.tar.gz");
     unlink("wwwdata.tar");
+
+    // Take items out of the github generated directory
+    $repo_name = extractRepoName(GIT_URL);
+    $branch = BRANCH;
+    pullSubData("wwwdata/$repo_name-$branch");
 
     return true;
 }
